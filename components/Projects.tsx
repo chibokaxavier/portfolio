4d278609bd89af1ea8c63648c00ec37a84e48495
manlikexavy <chibokaxavier@gmail.com>
2023-05-31 00:45:12 -0700

    EDITED THE PROJECTS SECTION

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,6 @@ import projects from "@/data2";
 import Link from "next/link";
 import { uuid } from "uuidv4";
 
-
 type Props = {};
 
 const Projects = (props: Props) => {
@@ -22,10 +21,10 @@ const Projects = (props: Props) => {
         Projects
       </h3>
 
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x h-screen  snap-mandatory z-20 pt-10 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x h-[650px]  snap-mandatory z-20 pt-10 scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin">
         {" "}
         {project.map((oneProject, i) => (
-          <Link   key={uuid()} href={oneProject.link} target="_blank">
+          <Link key={uuid()} href={oneProject.link} target="_blank">
             <div
               key={uuid()}
               className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 "
@@ -38,23 +37,31 @@ const Projects = (props: Props) => {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1.5 }}
                 viewport={{ once: true }}
-                className=" h-[200px] w-[200px] md:w-[300px] md:-[300px] object-contain"
+                className=" h-[200px] w-[200px] md:w-[300px] md:-[300px] object-contain "
                 src={oneProject.img}
                 alt=""
-            
               />
 
               <div className="px-0 md:px-10  max-w-6xl space-y-4 lg:space-y-10  ">
-                <h4 className=" text-2xl md:text-4xl font-semibold text-center uppercase
-                 ">
+                <h4
+                  className=" text-2xl md:text-4xl font-semibold text-center uppercase
+                 "
+                >
                   <span className=""></span>
                   {oneProject.title}
                 </h4>
-<div className="flex justify-evenly">
-    {oneProject.technologies.map((tech)=>{
-        return <img key={uuid()} src={tech} alt=""  className="h-10 w-10 lg:w-20 lg:h-20"/>
-    })}
-</div>
+                <div className="flex justify-evenly">
+                  {oneProject.technologies.map((tech) => {
+                    return (
+                      <img
+                        key={uuid()}
+                        src={tech}
+                        alt=""
+                        className="h-10 w-10 lg:w-20 lg:h-20"
+                      />
+                    );
+                  })}
+                </div>
                 <p className="text-lg text-center md:text-left font-semibold">
                   {oneProject.desc}
                 </p>
